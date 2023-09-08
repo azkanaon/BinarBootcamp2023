6956f2418939b88933bd164a5cefc540f62632f5
Muhammad Azka Atqiya <90915678+azkanaon@users.noreply.github.com>
2023-09-08 14:12:45 +0700

    edit dikit, ada kesalahpahaman

--- Challenge-02/nomor5.js
+++ Challenge-02/nomor5.js
@@ -31,97 +31,115 @@ const dataPenjualanNovel = [
     namaProduk: 'Laskar Pelangi',
     penulis: 'Andrea Hirata',
     hargaBeli: 55000,
-    hargaJual: 68000,
+    hargaJual: "68000",
     totalTerjual: 20,
     sisaStok: 56,
   },
 ];
 
-// variabel untuk menampung keuntunganPenjualan dan modalPenjualan
-let keuntungan = 0;
-let modal = 0;
-
-// loop untuk mengambil nilai untung dan modal
-dataPenjualanNovel.map((value) => {
-  // setiap kali loop, keuntungan bertambah
-  keuntungan += (value.hargaJual - value.hargaBeli) * value.totalTerjual; 
-  // setiap kali loop, modal bertambah
-  modal += value.hargaBeli * (value.totalTerjual + value.sisaStok);
-});
-
-// menentukan persentase keuntungan
-// rumus yang saya gunakan adalah keuntungan dibagi modal dikali 100%
-// toFixed(2) digunakan agar hanya ada 2 angka dibelakang koma
-const persentaseKeuntungan = ((keuntungan / modal) * 100).toFixed(2);
 
 
-// mencari produk terlaris (dilihat dari jumlah buku yang terjual)
-// menggunakan reduce karena lebih enak jika untuk perbandingan
-const produkTerlaris = dataPenjualanNovel.reduce((maxProduk, produk) => {
-  // jika index saat ini lebih besar totalJualnya dari index sebelumnya, maka return value dengan index saat ini
-  if (produk.totalTerjual > maxProduk.totalTerjual) {
-    return produk;
-  } else {
-    return maxProduk;
+const getInfoPenjualan = (data) => {
+
+  // variabel untuk menampung keuntunganPenjualan dan modalPenjualan
+  let keuntungan = 0;
+  let modal = 0;
+  let flag = false;
+  // loop untuk mengambil nilai untung dan modal
+  data.map((value) => {
+    // cek tipe data hargaBeli dan hargaJual
+    if(typeof(value.hargaBeli) !== 'number' || typeof(value.hargaJual) !== 'number'){
+      // cek apakah masukkan sebuah angka atau huruf
+      // jika huruf maka error
+      if(!isNaN(value.hargaJual) && !isNaN(value.hargaBeli)){
+        // ubah nilai menjadi tipe number
+        value.hargaJual = parseInt(value.hargaJual)
+        value.hargaBeli = parseInt(value.hargaBeli)
+      }else{
+        flag = true;
+      }
+      value.hargaBeli = parseInt(value.hargaBeli)
+    }
+    // setiap kali loop, keuntungan bertambah
+    keuntungan += (value.hargaJual - value.hargaBeli) * value.totalTerjual; 
+    // setiap kali loop, modal bertambah
+    modal += value.hargaBeli * (value.totalTerjual + value.sisaStok);
+  });
+
+  // pemberitahuan bahwa masukkan bukan angka
+  if(flag){
+    return "Harga Barang / Harga Jual bukan sebuah angka";
   }
-  // penggunaan dataPenjualanNovel[0] untuk menginisialisasi nilai maxProduk
-}, dataPenjualanNovel[0]);
 
-// untuk melakukan perhitungan totalJual untuk setiap nama penulis
-const dataPenulis = dataPenjualanNovel.reduce((acc, currentValue)=>{
-  // mencari apakah data akumulator(acc) sudah memiliki penulis dengan nilai yang sama dengan currentValue.penulis
-  const cekData = acc.find(item=>item.penulis === currentValue.penulis);
-  // jika ada, maka masukkan totalJual dari currentValue ke cekData totalJual
-  if(cekData){
-    cekData.totalTerjual += currentValue.totalTerjual;
-  // jika tidak ada, maka buat object baru dengan penulis dan totalJual dari currentValue
-  }else{
-    acc.push({penulis: currentValue.penulis, totalTerjual: currentValue.totalTerjual});
-  }
-  return acc;
-  // penggunaan [] sebagai inisialisasi acc
-}, [])
+  // menentukan persentase keuntungan
+  // rumus yang saya gunakan adalah keuntungan dibagi modal dikali 100%
+  // toFixed(2) digunakan agar hanya ada 2 angka dibelakang koma
+  const persentaseKeuntungan = ((keuntungan / modal) * 100).toFixed(2);
 
-// kode mirip dengan produkTerlaris tetapi untuk penulisTerlaris
-// penulisTerlaris dihitung dari penjualan buku dari setiap penulis
-const penulisTerlaris = dataPenulis.reduce((acc, current) =>{
-  if(current.totalTerjual > acc.totalTerjual){
-    return current;
-  }else{
+
+  // mencari produk terlaris (dilihat dari jumlah buku yang terjual)
+  // menggunakan reduce karena lebih enak jika untuk perbandingan
+  const produkTerlaris = data.reduce((maxProduk, produk) => {
+    // jika index saat ini lebih besar totalJualnya dari index sebelumnya, maka return value dengan index saat ini
+    if (produk.totalTerjual > maxProduk.totalTerjual) {
+      return produk;
+    } else {
+      return maxProduk;
+    }
+    // penggunaan data[0] untuk menginisialisasi nilai maxProduk
+  }, data[0]);
+
+
+
+  // untuk melakukan perhitungan totalJual untuk setiap nama penulis
+  const dataPenulis = data.reduce((acc, currentValue)=>{
+    // mencari apakah data akumulator(acc) sudah memiliki penulis dengan nilai yang sama dengan currentValue.penulis
+    const cekData = acc.find(item=>item.penulis === currentValue.penulis);
+    // jika ada, maka masukkan totalJual dari currentValue ke cekData totalJual
+    if(cekData){
+      cekData.totalTerjual += currentValue.totalTerjual;
+    // jika tidak ada, maka buat object baru dengan penulis dan totalJual dari currentValue
+    }else{
+      acc.push({penulis: currentValue.penulis, totalTerjual: currentValue.totalTerjual});
+    }
     return acc;
-  }
-})
+    // penggunaan [] sebagai inisialisasi acc
+  }, [])
+  // kode mirip dengan produkTerlaris tetapi untuk penulisTerlaris
+  // penulisTerlaris dihitung dari penjualan buku dari setiap penulis
+  const penulisTerlaris = dataPenulis.reduce((acc, current) =>{
+    if(current.totalTerjual > acc.totalTerjual){
+      return current;
+    }else{
+      return acc;
+    }
+  })
 
-// konversi nilai number ke mata uang IDR
-const rupiah = (number)=>{
-  return new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(number);
+  // konversi nilai number ke mata uang IDR
+  const rupiah = (number)=>{
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      // agar tidak ada ,00 pada mata uang
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(number);
+  }
+
+  // proses konversi
+  const keuntunganVersiRupiah = rupiah(keuntungan)
+  const modalVersiRupiah = rupiah(modal)
+
+  // output
+  return {
+    totalKeuntungan : keuntunganVersiRupiah,
+    totalModal : modalVersiRupiah,
+    persentaseKeuntungan : `${persentaseKeuntungan}%`,
+    produkBukuTerlaris : produkTerlaris.namaProduk,
+    penulisTerlaris : penulisTerlaris.penulis
+  }
 }
 
-// proses konversi
-const keuntunganVersiRupiah = rupiah(keuntungan)
-const modalVersiRupiah = rupiah(modal)
-
-// variabel akhir
-let finalResult = {}
-// memasukkan semua nilai yang didapat ke final resultq
-finalResult.totalKeuntungan = keuntunganVersiRupiah;
-finalResult.totalModal = modalVersiRupiah;
-finalResult.persentaseKeuntungan = `${persentaseKeuntungan}%`;
-finalResult.produkBukuTerlaris = produkTerlaris.namaProduk;
-finalResult.penulisTerlaris = penulisTerlaris.penulis;
 
 // print hasil akhir
-console.log(finalResult);
-
-
-
-
-const angka = [1,2,3,4,5]
-const result = angka.map(value => value**2)
-
-console.log(result);
+console.log(getInfoPenjualan(dataPenjualanNovel));
